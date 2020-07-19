e4616e77019760c0d3d8f152fd19aefec2ba5dff
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-07-20 00:23:43 +0300

    Click function to build the formation
    bulding the requierd formation of the web by a function of click on the add button.
    Reset the input value, and set the other contents of date and the input value.

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const ul = document.createElement('ul');
+const section = document.querySelector('.view');
+section.appendChild(ul);
+let input = document.querySelector('input[type="text"]');
+const AButton = document.getElementById('addButton');
+
+AButton.addEventListener('click', function(){
+    if (input.value!=="") {
+        // create li with main div with 3 divs.
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        div = document.createElement('div')
+        div.className = "todoContainer";
+        li.appendChild(div);
+        let priority = document.createElement('div');
+        priority.className = "todoPriority";
+        let date = document.createElement('div');
+        date.className = "todoCreatedAt";
+        let text = document.createElement('div');
+        text.className = "todoText";
+        div.appendChild(priority);
+        div.appendChild(date);
+        div.appendChild(text);
+        
+        // set the values of the divs
+        priority.textContent = document.getElementById('prioritySelector').value;
+        
+        //turn the JS date to SQL date
+        date.textContent = new Date().toLocaleString();
+        text.textContent = input.value;
+        // reset the input value after clicking
+        input.value = ""; 
+    }
+});
